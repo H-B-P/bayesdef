6ebcfbabd9bc1e55c914ef1d60ac2fecefeb2cfa
no no <no@no.no>
2018-08-27 17:28:44 +0100

    probdef is dead, long live bayesdef

---
+++ desktop/src/com/bayesdef/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bayesdef.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bayesdef.BayesDef;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BayesDef(), config);
+	}
+}
